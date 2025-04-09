90d84cb74953670584e15aa1fc37f405bdcd327a
ogiki477 <omojick88moses@gmail.com>
2025-04-09 10:52:17 +0300

    feat: Added the general layout of the app

---
+++ resources/views/admin/admin-layouts/sidebar.blade.php
@@ -0,0 +1,68 @@
+<!--begin::Sidebar-->
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+    <!--begin::Sidebar Brand-->
+    <div class="sidebar-brand">
+      <!--begin::Brand Link-->
+      <a href="./index.html" class="brand-link">
+        <!--begin::Brand Image-->
+        <img
+          src="../../dist/assets/img/comfarnet/logo.png"
+          alt="Pos logo"
+          class="brand-image opacity-75 shadow"
+          height="100px"
+          width="100px"
+        />
+        <!--end::Brand Image-->
+        <!--begin::Brand Text-->
+        <span class="brand-text fw-light">COMFARNET</span>
+        <!--end::Brand Text-->
+      </a>
+      <!--end::Brand Link-->
+    </div>
+    <!--end::Sidebar Brand-->
+    <!--begin::Sidebar Wrapper-->
+    <div class="sidebar-wrapper">
+      <nav class="mt-2">
+        <!--begin::Sidebar Menu-->
+        <ul
+          class="nav sidebar-menu flex-column"
+          data-lte-toggle="treeview"
+          role="menu"
+          data-accordion="false"
+        >
+          <li class="nav-item">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon bi bi-speedometer"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon bi bi-person"></i>
+              <p>Farmers</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon bi bi-cloud-snow-fill"></i>
+              <p>Gardens</p>
+            </a>
+          </li>
+          
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon bi bi-sliders"></i>
+              <p>System Settings</p>
+            </a>
+          </li>
+          
+          
+
+        <!--end::Sidebar Menu-->
+      </nav>
+    </div>
+    <!--end::Sidebar Wrapper-->
+  </aside>
+  <!--end::Sidebar-->
